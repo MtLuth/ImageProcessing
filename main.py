87a1cffb700a07f45b8da2bd3c8eaf38bc96ae8e
Nam1392003 <117732946+Nam1392003@users.noreply.github.com>
2024-04-11 11:52:04 +0700

    giaodien

--- main.py
+++ main.py
@@ -1,16 +1,156 @@
-from tkinter import Tk, Canvas
+import cv2
+import tkinter as tk
+from tkinter import Canvas, Frame, Button, Label, Entry, filedialog
+from PIL import Image, ImageTk
+import time
 
 class MainDisplay:
-
     def __init__(self):
-        self.width = 1200
-        self.height = 800
-        self.root = Tk()
+        self.width = 1500
+        self.height = 900
+        self.root = tk.Tk()
         self.root.title("Image Processing")
         self.root.geometry(str(self.width) + "x" + str(self.height))
+        
+        self.main_label = Label(self.root)
+        self.main_label.pack(side='left')
+        
+        self.upper_frame = Frame(self.main_label, width=1000, height=400)
+        self.upper_frame.pack(side='top')
+        
+        self.upper_left_frame = Frame(self.upper_frame, width=500, height=400)
+        self.upper_left_frame.pack(side='left')
+        
+        self.upper_right_frame = Frame(self.upper_frame, width=500, height=400)
+        self.upper_right_frame.pack(side='left')
+        
+        self.upper_input_image = Canvas(self.upper_left_frame, width=500, height=400, bg='white')
+        self.upper_input_image.pack()
+        
+        self.camera_button = Button(self.upper_right_frame, text='Chụp Camera 0', command=self.capture_image)
+        self.camera_button.pack(side='right')
+        
+        self.upper_camera_image = Canvas(self.upper_right_frame, width=500, height=400, bg='white')
+        self.upper_camera_image.pack()
+        
+        self.lower_frame = Frame(self.main_label, width=1000, height=400)
+        self.lower_frame.pack(side='top')
+        
+        self.lower_left_frame = Frame(self.lower_frame, width=500, height=400)
+        self.lower_left_frame.pack(side='left')
+        
+        self.lower_right_frame = Frame(self.lower_frame, width=500, height=400)
+        self.lower_right_frame.pack(side='left')
+        
+        self.lower_input_image = Canvas(self.lower_left_frame, width=500, height=400, bg='white')
+        self.lower_input_image.pack()
+        
+        self.camera_button_lower = Button(self.lower_right_frame, text='Chụp Camera 1', command=self.capture_image_lower)
+        self.camera_button_lower.pack(side='right')
+        
+        self.lower_camera_image = Canvas(self.lower_right_frame, width=500, height=400, bg='white')
+        self.lower_camera_image.pack()
+        
+        self.clock_label = Label(self.root, text="", font=("Arial", 50), fg="blue")
+        self.clock_label.pack(side='top', pady=(50, 0))
+        self.update_clock()
+        
+        self.parking_fee_label = Label(self.root, text="Tiền gửi xe: 0 VNĐ", font=("Arial", 20))
+        self.parking_fee_label.pack(side='top', pady=(20, 0))
+        
+        self.card_number_entry = Entry(self.root, font=("Arial", 20))
+        self.card_number_entry.pack(side='top', pady=(20, 0))
+        
+        self.parking_info_label = Label(self.root, text="Chỗ đậu xe còn trống: 50", font=("Arial", 15))
+        self.parking_info_label.pack(side='top', pady=(20, 0))
+        
+        self.cap_0 = cv2.VideoCapture(0)
+        self.cap_1 = cv2.VideoCapture(1)
+        
+        self.video_capture = None
+        self.selected_video_path = None
+        self.video_selected = False
+        self.video_frame = None  # Biến để lưu trữ frame từ video
+        self.open_video_button = Button(self.root, text='Chọn Video', command=self.open_video)
+        self.open_video_button.pack(side='top', pady=(20, 0))
+        
+        self.update_camera()
+        
+    def open_video(self):
+        self.selected_video_path = filedialog.askopenfilename()
+        self.video_capture = cv2.VideoCapture(self.selected_video_path)
+        self.video_selected = True
+        self.update_video()
+        
+    def update_video(self):
+        if self.video_selected and self.video_capture is not None and self.video_capture.isOpened():
+            ret, frame = self.video_capture.read()
+            if ret:
+                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                self.video_frame = Image.fromarray(img)  # Lưu trữ frame từ video
+                img = ImageTk.PhotoImage(image=self.video_frame)
 
+                self.upper_input_image.create_image(0, 0, anchor='nw', image=img)
+                self.upper_input_image.image = img
+
+        self.root.after(10, self.update_video)
+        
+    def capture_image(self):
+        if not self.video_selected:
+            ret, frame = self.cap_0.read()
+            if ret:
+                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                img = Image.fromarray(img)
+                img = ImageTk.PhotoImage(image=img)
+
+                self.upper_camera_image.create_image(0, 0, anchor='nw', image=img)
+                self.upper_camera_image.image = img
+        else:
+            if self.video_frame is not None:  # Kiểm tra xem đã có frame từ video hay không
+                img = ImageTk.PhotoImage(image=self.video_frame)
+
+                self.upper_camera_image.create_image(0, 0, anchor='nw', image=img)
+                self.upper_camera_image.image = img
+    
+    def capture_image_lower(self):
+        if not self.video_selected:
+            ret, frame = self.cap_1.read()
+            if ret:
+                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                img = Image.fromarray(img)
+                img = ImageTk.PhotoImage(image=img)
+            
+                self.lower_camera_image.create_image(0, 0, anchor='nw', image=img)
+                self.lower_camera_image.image = img
+            
+    def update_camera(self):
+        if not self.video_selected:
+            ret_0, frame_0 = self.cap_0.read()
+            if ret_0:
+                img_0 = cv2.cvtColor(frame_0, cv2.COLOR_BGR2RGB)
+                img_0 = Image.fromarray(img_0)
+                img_0 = ImageTk.PhotoImage(image=img_0)
+
+                self.upper_input_image.create_image(0, 0, anchor='nw', image=img_0)
+                self.upper_input_image.image = img_0
+
+            ret_1, frame_1 = self.cap_1.read()
+            if ret_1:
+                img_1 = cv2.cvtColor(frame_1, cv2.COLOR_BGR2RGB)
+                img_1 = Image.fromarray(img_1)
+                img_1 = ImageTk.PhotoImage(image=img_1)
+
+                self.lower_input_image.create_image(0, 0, anchor='nw', image=img_1)
+                self.lower_input_image.image = img_1
+            
+        self.root.after(10, self.update_camera)
+            
+    def update_clock(self):
+        current_time = time.strftime('%H:%M:%S')
+        self.clock_label.config(text=current_time)
+        self.root.after(1000, self.update_clock)
+        
     def run(self):
-        self.input_image = Canvas()
         self.root.mainloop()
 
 if __name__ == '__main__':
